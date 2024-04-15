2d05d71d4efba5feca3535aefa2fd667008ee0f3
Ruben Ramos <ruben.ramos@cifpaviles.net>
2024-04-15 16:09:20 +0200

    Añadido esqueleto Spring Boot

---
+++ src/main/java/com/ciber03/calculator03/Calculator03Application.java
@@ -0,0 +1,13 @@
+package com.ciber03.calculator03;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Calculator03Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Calculator03Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/ciber03/calculator03/Calculator03ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ciber03.calculator03;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Calculator03ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
