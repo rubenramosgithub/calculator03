26ef868a054d0a47e9dbd6f14b3697c4bf7c41f0
Ruben Ramos <ruben.ramos@cifpaviles.net>
2024-04-18 18:11:33 +0200

    Se añaden suma, controlador y test unitario

---
+++ src/main/java/com/ciber03/calculator03/Calculator.java
@@ -0,0 +1,9 @@
+package com.ciber03.calculator03;
+import  org.springframework.stereotype.Service;
+
+@Service
+public class Calculator {
+    int sum(int a, int b) {
+         return a + b;
+    }
+}
---
+++ src/test/java/com/ciber03/calculator03/CalculatorApplicationTests.java
@@ -0,0 +1,20 @@
+package com.ciber03.calculator03;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import static org.junit.Assert.assertEquals;
+
+@SpringBootTest
+public class CalculatorApplicationTests {
+
+	private Calculator calculator = new Calculator();
+
+	@Test
+	void contextLoads() {
+	}
+
+	@Test
+	public void testSum() {
+		assertEquals(5, calculator.sum(2, 3));
+	}
+
+}
