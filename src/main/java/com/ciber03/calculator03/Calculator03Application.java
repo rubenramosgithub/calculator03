c2bfbbced3eb74337e48be843dea2f6999473443
Ruben Ramos <ruben.ramos@cifpaviles.net>
2024-04-26 17:51:03 +0200

    Añadido checkstyle2

--- src/main/java/com/ciber03/calculator03/Calculator03Application.java
+++ src/main/java/com/ciber03/calculator03/Calculator03Application.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Calculator03Application {
 
-	public static void main(String[] args) {
-		SpringApplication.run(Calculator03Application.class, args);
-	}
+    public static void main(String[] args) {
+       SpringApplication.run(Calculator03Application.class, args);
+    }
 
 }
